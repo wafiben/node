369d5b261f95e6b911d5cdbffea35dda84067aae
Wafi benjeddou <wafibenjeddou@gmail.com>
2021-08-07 12:53:20 +0200

    first commit

---
+++ filtered-ls.js
@@ -0,0 +1,16 @@
+const fs =require('fs');
+const path=require('path')
+var myPath=process.argv[2];
+
+var res=[]
+fs.readdir(myPath,(error,files)=>{
+  files.filter((element)=>{
+      if(path.parse(element).ext===("."+process.argv[3]))
+      console.log(element)
+      return null
+    })
+
+
+  
+    
+})
